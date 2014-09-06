2a93eeeeef112b400e55da04cccaca068b4a0736
Manish Mulimani <manish@mobilitas.co>
2014-09-07 00:14:28 +0530

    Initial commit.

---
+++ gen/com/example/android/weatherlistwidget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.android.weatherlistwidget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
